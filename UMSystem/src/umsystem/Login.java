93b3e26a73b4a06ff44fa00d20585c5a9c8238fa
Fairuz Rahaman Chowdhury <frcshovon@gmail.com>
2020-06-24 02:29:12 +0600

    completed splash screen and started login frame

---
+++ UMSystem/src/umsystem/Login.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package umsystem;
+
+/**
+ *
+ * @author SHOVON
+ */
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Login extends JFrame implements ActionListener {
+    JLabel l1,l2;
+    JTextField jTF1;
+    JPasswordField jPF1;
+    JButton jB1,jB2;
+    
+    Login(){
+        super("Login");
+        
+        setLayout(null); /// custom layout 
+         getContentPane().setBackground(Color.WHITE); ///getContentPane is used to apply something in the whole frame
+         l1 =new JLabel ("ID" );
+         l1.setBounds(40, 20, 100, 30);
+         add(l1);
+         
+         l2= new JLabel("Password");
+         l2.setBounds(40,70,100,30);
+         add(l2);
+         
+         jTF1 = new JTextField();
+         jTF1.setBounds(150,20,150,30);
+         add(jTF1);
+         
+         jPF1= new JPasswordField();
+         jPF1.setBounds(150,70,150,30);
+         add(jPF1);
+         
+         ImageIcon imageIcon1 = new ImageIcon(ClassLoader.getSystemResource("umsystem/projectsicons/second.jpg"));
+         Image image= imageIcon1.getImage().getScaledInstance(200, 200, Image.SCALE_DEFAULT);
+         ImageIcon imageIcon2 = new ImageIcon(image);
+         
+         JLabel jLabel = new JLabel(imageIcon2);
+         jLabel.setBounds(350,20,150,150);
+         add(jLabel);
+         
+         jB1= new JButton("Login");
+         jB1.setBounds(40,140,120,30);
+         jB1.setFont(new Font("serif",Font.BOLD,15));
+         jB1.addActionListener(this);
+         jB1.setBackground(Color.BLACK);
+         jB1.setForeground(Color.WHITE);
+         add(jB1);
+         
+         jB2= new JButton("Cancel");
+         jB2.setBounds(180,140,120,30);
+         jB2.setFont(new Font("serif",Font.BOLD,15));
+         jB2.addActionListener(this);
+         jB2.setBackground(Color.BLACK);
+         jB2.setForeground(Color.WHITE);
+         add(jB2);
+         
+         setVisible(true);
+         setSize(600,300);
+         setLocation(500,300);
+         
+         
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        if(ae.getSource()==jB1){
+            try{
+
+            }catch(Exception e){
+
+             }
+        }else if (ae.getSource()==jB2){
+            try{
+                   System.exit(0);
+            }catch(Exception e){
+                    
+             }
+        }
+       
+    }
+    
+    public static void main(String args[]){
+        new Login();
+    }
+   
+}
--- UMSystem/src/umsystem/Splash.java
+++ UMSystem/src/umsystem/Splash.java
@@ -5,26 +5,65 @@
  */
 package umsystem;
 
-import javax.swing.JFrame;
+import java.awt.*;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.*;
 
 /**
  *
  * @author SHOVON
  */
 public class Splash {
-    
+        public static void main(String args[]){
+            Frame frame = new Frame();
+            frame.setVisible(true);
+            
+            for(int i=2,x=1;i<=600;i+=4,x++){ 
+                frame.setLocation(800-(i+x)/2, 300-x/2);
+                frame.setSize(i+3*x , i+x /2 );
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(Splash.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                
+            }
+            
+    }
 }
 
 
 class Frame extends JFrame implements Runnable{
-
-    @Override
-    public void run() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+    Thread t1;
     public Frame (){
-        super("UMS");
+        super("UMS"); /// this mainly shows the tilte of frame 
+        setLayout(new FlowLayout());
+        ImageIcon imageIcon1= new ImageIcon( ClassLoader.getSystemResource("umsystem/projectsicons/splashscreen.jpg"));
+        Image image1= imageIcon1.getImage().getScaledInstance(1000, 700, Image.SCALE_DEFAULT);
+        ImageIcon imageIcon2= new ImageIcon(image1);
+        
+        JLabel jLabel = new JLabel(imageIcon2);
+        add(jLabel);
+//        Runnable r= new Frame();
+//        t1= new Thread(r);
+//        t1.start();
+          t1= new Thread(this);
+          t1.start();
+        
     }
+     @Override
+    public void run() {
+        try{
+            Thread.sleep(5000);
+            setVisible(false);
+            new Login();
+        }catch(Exception e){
+            e.printStackTrace();
+        } 
+    }
+    
     
     
 }
